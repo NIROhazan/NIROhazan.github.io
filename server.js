3b23ea4ea5895db374e69a07612542fa055ecde3
NIROhazan <156203893+NIROhazan@users.noreply.github.com>
2024-06-08 00:33:28 +0300

    Add files via upload

---
+++ server.js
@@ -0,0 +1,122 @@
+const express = require("express");
+const mongojs = require("mongojs");
+const { userInfo } = require("os");
+const path = require("path");
+
+// MongoDB connection
+const db = mongojs(
+  "mongodb+srv://Student:webdev2024student@cluster0.uqyflra.mongodb.net/webdev2024",
+  ["mitzinet_<Nir_Hazan_Omer_Bisdush>"]
+);
+
+const users = db.collection("mitzinet_<Nir_Hazan_Omer_Bisdush>");
+
+const app = express();
+app.use(express.json()); // Middleware to parse JSON body
+app.use(express.urlencoded({ extended: true }));
+
+// Serve static files from the 'static' directory
+app.use(express.static(path.join(__dirname, "static")));
+
+// Check if email exists
+app.post("/check-email", (req, res) => {
+  const email = req.body.email;
+  users.findOne({ email: email }, (err, user) => {
+    if (err) {
+      console.error("Database query error:", err);
+      res.status(500).send("Database error");
+    } else if (user) {
+      res.status(409).send("Email already exists");
+      return false;
+    } else {
+      res.status(200).send("Email is available");
+      return true;
+    }
+  });
+});
+// Handle form submission
+app.post("/register", (req, res) => {
+  const { firstName, lastName, phoneNumber, email, password, confirmPassword } =
+    req.body;
+
+  // Validation checks
+  if (!email || !email.includes("@") || !email.includes(".")) {
+    return res
+      .status(400)
+      .send(
+        '<script>alert("Invalid email format"); window.location.href="/";</script>'
+      );
+  }
+  if (!firstName || !lastName) {
+    return res
+      .status(400)
+      .send(
+        '<script>alert("must be first and last name"); window.location.href="/";</script>'
+      );
+  }
+  //
+  if (password.length < 8) {
+    return res
+      .status(400)
+      .send(
+        '<script>alert("Password must be at least 8 characters long"); window.location.href="/";</script>'
+      );
+  }
+  if (password != confirmPassword) {
+    return res
+      .status(400)
+      .send(
+        '<script>alert("Passwords not match"); window.location.href="/";</script>'
+      );
+  }
+
+  // Check if email already exists
+  users.findOne({ email: email }, (err, doc) => {
+    if (err) {
+      console.error("Error saving user to database:", err);
+      return res
+        .status(400)
+        .send(
+          '<script>alert("User registration failed"); window.location.href="/";</script>'
+        );
+    }
+    if (doc) {
+      return res
+        .status(400)
+        .send(
+          '<script>alert("Email already registered"); window.location.href="/";</script>'
+        );
+    } else {
+      // If all validations pass and email doesn't exist, insert the new user
+      const newUser = {
+        firstName: firstName,
+        lastName: lastName,
+        phoneNumber: phoneNumber,
+        email: email,
+        password: password,
+      };
+
+      users.insert(newUser, (err, doc) => {
+        if (err) {
+          console.error("Error saving user to database:", err);
+          return res
+            .status(400)
+            .send(
+              '<script>alert("User registration failed"); window.location.href="/";</script>'
+            );
+        }
+        return res
+          .status(200)
+          .send(
+            '<script>alert("User registered successfully"); window.location.href="/";</script>'
+          );
+      });
+    }
+  });
+});
+
+// Start the server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
---
+++ static/MyScript.js
@@ -0,0 +1,118 @@
+document.addEventListener("DOMContentLoaded", function () {
+  alert("Welcome");
+  var registrationForm = document.getElementById("registrationForm");
+  registrationForm.addEventListener("submit", function (event) {
+    // Prevent the form from submitting initially in each part
+
+    var firstName = document.getElementById("firstName").value;
+    var lastName = document.getElementById("lastName").value;
+    var password = document.getElementById("password").value;
+    var confirmPassword = document.getElementById("confirmPassword").value;
+
+    // Validate first name
+    if (!firstName) {
+      event.preventDefault();
+      document
+        .getElementById("firstName")
+        .setCustomValidity("Must enter first name.");
+      return;
+    } else {
+      document.getElementById("firstName").setCustomValidity("");
+    }
+
+    // Validate last name
+    if (!lastName) {
+      event.preventDefault();
+      document
+        .getElementById("lastName")
+        .setCustomValidity("Must enter last name.");
+      return;
+    } else {
+      document.getElementById("lastName").setCustomValidity("");
+    }
+
+    // Validate password length
+    if (password.length < 8) {
+      event.preventDefault();
+      document
+        .getElementById("passwordError")
+        .setCustomValidity("Password must be at least 8 characters long.");
+      return;
+    } else {
+      document.getElementById("password").setCustomValidity("");
+    }
+
+    // Validate password match
+    if (password !== confirmPassword) {
+      event.preventDefault();
+      document
+        .getElementById("confirmPassword")
+        .setCustomValidity("Passwords don't match.");
+      return;
+    } else {
+      document.getElementById("confirmPassword").setCustomValidity("");
+    }
+
+    // If all validations pass, submit the form
+  });
+
+  // Event listener for email input blur to check if email exists
+  document.getElementById("email").addEventListener("blur", checkEmail);
+  document
+    .getElementById("deleteForm")
+    .addEventListener("submit", function (event) {
+      event.preventDefault();
+
+      const email = document.getElementById("email").value;
+      const password = document.getElementById("password").value;
+
+      const response = fetch("/delete-record", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ email: email, password: password }),
+      });
+
+      if (response.ok) {
+        alert("succsess");
+      } else {
+        alert("Error delete user ");
+      }
+    });
+});
+async function checkEmail() {
+  const emailInput = document.getElementById("email");
+  const email = emailInput.value;
+  const emailError = document.getElementById("emailError");
+
+  try {
+    const response = await fetch("/check-email", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ email: email }),
+    });
+    const data = await response.text();
+
+    if (response.ok) {
+      // Email is available
+      emailError.textContent = "";
+      emailInput.setCustomValidity("");
+      return true;
+    } else if (response.status === 409) {
+      // Email already exists
+      emailError.textContent = "Email already exists.";
+      emailInput.setCustomValidity("Email already exists.");
+      return false;
+    } else {
+      // Handle other errors
+      console.error("Server responded with error:", data);
+      return false;
+    }
+  } catch (error) {
+    console.error("Error:", error);
+    return false;
+  }
+}
